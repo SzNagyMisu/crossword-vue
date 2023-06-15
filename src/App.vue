35b253e2a0189eaca4e52c37718ba1f6c966b34e
mihalyszijjartonagy <mihaly.szijjartonagy@prezi.com>
2023-06-15 12:05:58 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script>
+import Cell from './components/Cell.vue';
+
+export default {
+  components: {Cell},
+  data() {
+    return {
+      rows: [
+        [{
+          value: 'A',
+          nr: 1,
+        }, {
+          value: 'B',
+        }, {
+          value: 'C',
+          nr: 2,
+        }, {
+          isBlack: true,
+    		}],
+        [{
+          value: 'A',
+          nr: 3,
+        }, {
+          isBlack: true,
+    		}, {
+          value: 'B',
+          nr: 4,
+        }, {
+          value: 'C',
+        }],
+      ]
+    }
+  }
+};
+</script>
+
+<template>
+  <section>
+    <section class="row" v-for="row in rows">
+      <Cell
+        v-for="(cell, cellIdx) in row"
+        :key="cellIdx"
+        v-bind="cell"
+      />
+    </section>
+  </section>
+</template>
+
+<style scoped>
+  section.row {
+    display: flex
+  }
+</style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  data () {
+    return {};
+  },
+  props: {
+    isBlack: Boolean,
+    value: [String, null],
+    nr: [Number, null],
+  },
+  mounted () {
+    // console.log('here');
+  }
+}
+</script>
+
+<template>
+  <article :class="{black: isBlack}">
+    <span class="nr" v-if="nr">{{ nr }}</span>
+    <span class="value" v-if="value">{{ value }}</span>
+  </article>
+</template>
+
+<style scoped>
+  article {
+    width: 30px;
+    height: 30px;
+    border: 1px solid black;
+    margin: -1px 0 0 -1px;
+    position: relative;
+  }
+  
+  article.black {
+    background: black;
+  }
+  
+  span.nr {
+    font-size: 9px;
+    padding: 1px;
+    position: absolute
+  }
+  
+  span.value {
+    display: inline-flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+  }
+</style>
